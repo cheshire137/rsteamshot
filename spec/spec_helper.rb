d0250ea4d8226846df0a33d6e97a6d50aef1d3ca
Sarah Vessels <cheshire137@gmail.com>
2017-10-28 13:21:39 -0500

    Configure vcr gem for tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,15 @@
-require "bundler/setup"
-require "rsteamshot"
+require 'bundler/setup'
+require 'rsteamshot'
+require 'vcr'
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'fixtures/vcr_cassettes'
+  config.hook_into :webmock
+end
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
+  config.example_status_persistence_file_path = '.rspec_status'
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
