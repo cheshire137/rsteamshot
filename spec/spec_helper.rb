e58c25bc413c875d3c5b1e3d732a13b2bfda7290
Sarah Vessels <cheshire137@gmail.com>
2017-10-29 14:14:28 -0500

    Run tests in random order each time

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,8 @@ VCR.configure do |config|
 end
 
 RSpec.configure do |config|
+  config.order = :random
+
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = '.rspec_status'
 
